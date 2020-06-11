d775adbfa20b7038b44746d525c622679a8ded4c
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-06-10 21:46:58 -0300

    adicionado autenticação de usuário

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Manter conectado')
     submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repetir Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Cadastrar')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Usuário não está disponível.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Já existe um usuário com este e-mail.')
+            
+            
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Vinicius'}
     posts = [
         {
             'author': {'username': 'João'},
@@ -18,14 +21,51 @@ def index():
             'body': 'fastapi é a melhor coisa que já vi!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Parabéns, você acaba de se registrar!')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Registrar', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requisitado por usuário {form.username.data},'
-              + f' manter_conectado={form.remember_me.data}')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user is None or not user.check_password(form.password.data):
+            flash('Nome de usuário ou senha inválidos')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc is not '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
+
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
