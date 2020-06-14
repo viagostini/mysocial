c90a6048a9ebbb29f53ad79d9f72d378a2530fda
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-06-13 21:17:25 -0300

    basic error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -14,4 +18,20 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/mysocial.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MySocial startup')
+
+
+from app import routes, models, errors
--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Usuário', validators=[DataRequired()])
     about = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
     submit = SubmitField('Editar Perfil')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Por favor escolha um nome diferente.')
